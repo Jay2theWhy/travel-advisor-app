db9e8a916192dd7da555282c05246dc815eb06b5
Jing Yang Fan <jyfan16@hotmail.com>
2022-07-04 00:02:09 -0400

    removed key

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -13,7 +13,7 @@ const Map = ({setCoordinates, setBounds, coordinates}) => {
     return (
         <div className={classes.mapContainer}>
             <GoogleMapReact 
-                bootstrapURLKeys={{key: 'AIzaSyCsvixRRHsCuobj9vI1yMlZ3UJpGb3Ftrs' }}
+                bootstrapURLKeys={{key: '' }}
                 defaultCenter={coordinates}
                 center={coordinates}
                 defaultZoom={14}
